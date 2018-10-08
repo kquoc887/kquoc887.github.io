2a5773a1205dbb977f8759bd493871ebed0b599f
Quoc Khanh <kquoc887@gmail.com>
2018-10-08 10:33:25 +0700

    edit folder BaiTap1JS

--- BaiTap1JS/js/bai1.js
+++ BaiTap1JS/js/bai1.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
-	$("#btn_add").click(function(){
-		var product_name = $("#name_product").val();
+	$("#name_product").attr("maxlength","30");
+	$("#btn_add-js").click(function(){
+		var product_name = $("#name_product").val().trim();
 		if (product_name == "") {
-			alert("Chưa nhập tên sản phẩm");
+			alert("Please enter product name");
 			return;
 		}
-		$(".list_product").append("<li>"+ $("#name_product").val()+
-		 	 "<input class='btn_remove' type='button' value='X'>"+"</li>");
+		$(".list_product-js").append("<li>"+ product_name+
+		 	 "<input class='btn_remove btn_remove-js' type='button' value='X'>"+"</li>");
+		$("#name_product").val("").focus();
+
 	});
 	$(document).on("click", ".btn_remove", function(){
 		$(this).parent().remove();
