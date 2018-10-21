f451a269dda9fd451ad6df91d4aa53c6cba1618e
Quoc Khanh <kquoc887@gmail.com>
2018-10-21 22:10:12 +0700

    edit filde action.js in folder BaiTap4JS(Tuan3)

--- BaiTap4JS(Tuan3)/js/action.js
+++ BaiTap4JS(Tuan3)/js/action.js
@@ -1,27 +1,62 @@
 $(document).ready(function() {
-function effect() {
-	var indexLeave = Math.floor((Math.random()*5)+1);
-	var positionFall = Math.floor(Math.random()*10 + 100);
-	if (indexLeave >1 && indexLeave < 6) {
-		 TweenMax.fromTo($("leave" + indexLeave),3,
-			{
-				// x: positionFall,
-				top:-500
-			},
-			{
-				top:500,
-				skewX: 350,
-				skewY: 50,
-				repeat:-1
-			}
-		)
+ 	var listLeaves = $(".js-leave");
+
+ 	/** function effectLeavesFall: create effect falling for each leaf in array "listLeaves" */
+	function effectLeavesFall() {
+		for (var i = 0; i < listLeaves.length; i++) {
+	 		var leavesFall = $(listLeaves[i]);
+	 		var valObject = randomAttributeValues();
+	 		leavesFall.css({left: valObject.left});
+	 		TweenMax.staggerTo(leavesFall, valObject.duration, {
+	 			x: valObject.distanceX,
+	 			y: 800,
+	 			rotation: valObject.rotation,
+	 			transformOrigin: "left top",
+	 			rotationX: valObject.roatationX,
+	 			rotationY: valObject.rotationY,
+	 			delay: valObject.timedelay,
+	 			onComplete: repeatEffect,
+	 			onCompleteParams: [leavesFall]
+	 		}, 0.3);
+	 	}
 	}
-	// console.log($("leave"+indexLeave));
-}
 
-setInterval(function() {
-	effect();
-},600)
+	/** 
+	 * function repeatEffect: repeat effect of  a leaves in array "listLeaves".
+	 * input: param leaves is a leaves in array "listLeaves". 
+	 */
 
-// console.log($(arrLeave[random()]));
+	function repeatEffect(leaves) {
+		TweenMax.set(leaves, {y:-100});
+		var valueOfAttribute = randomAttributeValues();
+		TweenMax.staggerTo(leaves, valueOfAttribute.duration, {
+			x:valueOfAttribute.distanceX,
+			y:800,
+			roatation: valueOfAttribute.rotation,
+			transformOrigin: "left top",
+			roatationX: valueOfAttribute.roatationX,
+			rotationY: valueOfAttribute.rotationY,
+			onComplete: repeatEffect,
+			onCompleteParams: [leaves]
+		});
+	}
+
+	/**
+	 * function randomAttributeValues: set value of attribute for leaf in array "listLeaves".
+	 */
+	function randomAttributeValues() {
+		var attributes = {
+			roatation: Math.floor(Math.random() *360),
+			roatationX: Math.floor(Math.random() * 360),
+			rotationY: Math.floor(Math.random() * 360),
+			distanceX:  Math.floor(Math.random() * 100),
+			timedelay: Math.random() * 10,
+			left: Math.floor(Math.random()  * $(".container").width()),
+			duration:3
+		};
+		return attributes;
+	}
+
+	console.log($(".container").width()*Math.random() );
+	effectLeavesFall();
 });
