a07654158180069d60f23e87b1fa12c8fc5c9059
Quoc Khanh <kquoc887@gmail.com>
2018-10-22 11:51:17 +0700

    edit file action.js in folder BaiTap3JSTuan3

--- BaiTap3JS(Tuan3)/js/actiom.js
+++ BaiTap3JS(Tuan3)/js/actiom.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
-	var cloud = function () {
+	var arrLayer = $(".layer img");
+	var cloud = function (left,layer) {
 		this.position = 0;
 		this.transfer = 0;
 		this.cloudEffect = function(e) {
 			if (this.position < e.pageX){
-				this.transfer += 0.8;
-				$(".layer").css({
+				this.transfer += left;
+				$(layer).css({
 					"display": "block",
 					"left": this.transfer
 				});
 			} else {
-				this.transfer -= 0.8;
-				$(".layer").css({
+				this.transfer -= left;
+				$(layer).css({
 					"display": "block",
 					"left": this.transfer
 				});
@@ -19,8 +20,12 @@ $(document).ready(function() {
 		this.position = e.pageX;
 		}
 	};
-	var myCloud = new cloud();
+	var myCloudLayer1 = new cloud(0.6,arrLayer[0]);
+	var myCloudLayer2 = new cloud(0.4,arrLayer[1]);
+	var myCloudLayer3 = new cloud(0.3,arrLayer[2]);
 	$(".container").mousemove(function(event) {
-		myCloud.cloudEffect(event);
+		myCloudLayer1.cloudEffect(event);
+		myCloudLayer2.cloudEffect(event);
+		myCloudLayer3.cloudEffect(event);
 	});
 });
